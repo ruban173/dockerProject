80e1fd86a81e89c987aa480ef1d6e54c01a98adb
Alexandr <ruban173@rambler.ru>
2022-01-11 17:31:26 +0300

    first commit

---
+++ node/scripts/node_start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+echo 'running prestart node script'
+echo 'running npm install'
+#npm install
+
+echo 'initialization done, start watching'
+#npm run dev
+#npm run watch-poll
